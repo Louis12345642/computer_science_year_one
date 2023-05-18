6a42a105ed08e0a76aabba72b8d31e1dc9c2e08f
kual Louis <mubaraklouis@gmail.com>
2023-05-18 16:18:38 +0200

    worked on the structures in c

---
+++ projects/data structures and algorithms in c/pointers/structures/index.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+//a structure is a collection of variable kinda like an object in javascript or a class
+
+struct player {
+char name[10];
+int age;
+};
+
+
+
+int main(){
+
+    struct player p;
+
+strcpy(p.name,"mkay");
+int age =p.age =6;
+struct player p1;
+
+p1.age=29;
+printf("%d",p1.age);
+
+printf("%d",age);
+printf("%s",p.name);
+
+
+    return 0;
+}
